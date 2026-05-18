86c79541e45b8500f4e561cf5eea633699d87dff
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2026-05-18 23:01:35 +0200

    Start basket page

--- picklio/app/Livewire/front/Basket.php
+++ picklio/app/Livewire/front/Basket.php
@@ -3,13 +3,79 @@
 namespace App\Livewire\front;
 
 use App\Livewire\PicklioComponent;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 
 class Basket extends PicklioComponent
 {
+    public $cart;
+
+    #[Computed]
+    public function orderItems()
+    {
+        if (!empty($this->userConnected)) {
+            $this->cart = Order::with(['orderItems', 'orderItems.product'])
+                ->where('account_id', $this->userConnected->account->id)
+                ->where('status', Order::INITCART)
+                ->first();
+            if (!empty($this->cart)) {
+                foreach ($this->cart->orderItems as $orderItem) {
+                    if ($orderItem->quantity == 0) {
+                        $orderItem->delete();
+                    }
+                }
+            } else {
+                return [];
+            }
+            if ($this->cart->orderItems->isEmpty()) {
+                $this->cart->delete();
+
+                return [];
+            }
+
+            return $this->cart->orderItems;
+        } else {
+            return [];
+        }
+    }
+
+    public function delete($orderItemId)
+    {
+        $orderItem = OrderItem::findOrFail($orderItemId);
+        if ($orderItem->delete()) {
+            $cart = Order::where('account_id', $this->userConnected->account->id)
+                ->where('status', Order::INITCART)
+                ->first();
+
+            if ($cart) {
+                $cart->total_price = $cart->orderItems()->sum(DB::raw('price'));
+                $cart->save();
+            }
+            unset($this->orderItems);
+            $this->dispatch('delete-order');
+        }
+    }
+
+    #[On('edit-product')]
+    public function resetTotalPrice()
+    {
+        if (!empty($this->userConnected)) {
+            $this->cart = Order::with(['orderItems', 'orderItems.product'])
+                ->where('account_id', $this->userConnected->account->id)
+                ->where('status', Order::INITCART)
+                ->first();
+        }
+    }
+
     public function render()
     {
         return view('livewire.front.basket')
             ->layout('layouts.front')
-            ->title(__('commons.pageName.front.basket').' | Picklio');
+            ->title(__('commons.pageName.front.basket') . ' | Picklio');
     }
+
+
 }
--- picklio/resources/views/livewire/front/basket.blade.php
+++ picklio/resources/views/livewire/front/basket.blade.php
@@ -1,3 +1,88 @@
 <div class="basket">
-    <h1>{{__('commons.pageName.front.basket')}}</h1>
+    <section x-data="{ deleteOrder: false }"
+             x-on:delete-order.window="deleteOrder = true; setTimeout(() => deleteOrder = false, 3000)"
+             class="basket__container paddingMedia">
+        <h2 class="basket__container__title">Votre panier</h2>
+        <div class="basket__container__basketContainer">
+            <div class="basket__container__basketContainer__itemContainer">
+                @forelse($this->orderItems as $orderItem)
+                    <div wire:key="orderItem-{{ $orderItem->id }}" class="basket__container__basketContainer__itemContainer__card">
+                        <div class="basket__container__basketContainer__itemContainer__card__imgContainer">
+                            @if($orderItem->product->picture_path == 'images/missing-product.webp')
+                                <img src="{{asset($orderItem->product->picture_path)}}" alt=""
+                                     class="basket__container__basketContainer__itemContainer__card__imgContainer__img">
+                            @else
+                                <img
+                                    src="{{ $orderItem->product->pictureUrl(600) }}"
+                                    srcset="{{ $orderItem->product->pictureUrl(300) }} 300w, {{ $orderItem->product->pictureUrl(600) }} 600w,{{ $orderItem->product->pictureUrl(900) }} 900w"
+                                    sizes="(max-width: 400px) 300px, (max-width: 700px) 600px, 900px"
+                                    alt="{{$orderItem->product->name}}"
+                                    class="basket__container__basketContainer__itemContainer__card__imgContainer__img">
+                            @endif
+                        </div>
+                        <p class="basket__container__basketContainer__itemContainer__card__name"><a
+                                href="{{ route('front.catalogue.show', $orderItem->product->id) }}"
+                                class="basket__container__basketContainer__itemContainer__card__name__link">{{$orderItem->product->name}}</a>
+                        </p>
+                        <livewire:front.components.shop-card
+                            :price="$orderItem->priceFormatted"
+                            :productId="$orderItem->product->id"
+                            :card="true"
+                            :basket="true"
+                            :quantity="$orderItem->quantity"
+                        />
+
+                        <x-svg.svg wire:click="delete({{$orderItem->id}})"
+                                   class="basket__container__basketContainer__itemContainer__card__delete__svg"
+                                   name="trash"/>
+                    </div>
+                @empty
+                    <div class="basket__container__basketContainer__itemContainer__card">
+                        <p class="basket__container__basketContainer__itemContainer__card__empty">Vous n’avez rien dans
+                            votre panier @if(empty($this->userConnected))
+                                <a href="{{ route('auth.login') }}"
+                                   class="basket__container__basketContainer__itemContainer__card__empty__link">Connectez-vous</a>
+                            @endif </p>
+                    </div>
+                @endforelse
+                <div
+                    x-show="deleteOrder"
+                    x-transition
+                    class="toast"
+                    x-cloak
+                >
+                    {{__('front.order.toast.remove.success')}}
+                </div>
+            </div>
+            @if(!empty($this->orderItems))
+                <aside class="basket__container__basketContainer__priceContainer">
+                    <h3 class="basket__container__basketContainer__priceContainer__title">Total de votre panier</h3>
+                    <hr class="basket__container__basketContainer__priceContainer__hr">
+                    <div class="basket__container__basketContainer__priceContainer__valueContainer">
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__title">Prix
+                            HTVA</p>
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__value"></p>
+                    </div>
+                    <div class="basket__container__basketContainer__priceContainer__valueContainer">
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__title">TVA
+                            21%*</p>
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__value"></p>
+                    </div>
+                    <hr class="basket__container__basketContainer__priceContainer__hr">
+                    <div class="basket__container__basketContainer__priceContainer__valueContainer">
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__title">Total</p>
+                        <p class="basket__container__basketContainer__priceContainer__valueContainer__value">{{$orderItem->order->priceFormatted}}</p>
+                    </div>
+                    <button class="button button--icon basket__container__basketContainer__priceContainer__button">
+                        Continuer
+                        <x-svg.svg class="basket__container__basketContainer__priceContainer__button__svg"
+                                   name="arrow"/>
+                    </button>
+                    <p class="basket__container__basketContainer__priceContainer__condition">*21% par défaut et 6%
+                        produit alimentaire</p>
+                </aside>
+            @endif
+        </div>
+
+    </section>
 </div>
