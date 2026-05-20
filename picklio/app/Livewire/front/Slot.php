cf1bd42f2fb2cf0cedcb42372400cba56dabf921
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2026-05-20 16:10:41 +0200

    Add Slot page

--- picklio/app/Livewire/front/Slot.php
+++ picklio/app/Livewire/front/Slot.php
@@ -2,14 +2,111 @@
 
 namespace App\Livewire\front;
 
+use App\Livewire\form\front\ChooseSlotForm;
 use App\Livewire\PicklioComponent;
+use App\Models\Order;
+use App\Models\PickupSlot;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Livewire\Attributes\Computed;
 
 class Slot extends PicklioComponent
 {
+    public $today;
+
+    public Order $order;
+
+    public $days;
+
+    public $selectedWeek = 'current';
+
+    public ChooseSlotForm $form;
+
+    public function mount(Order $order): void
+    {
+        $this->order = $order;
+        $this->today = Carbon::now();
+        if ($this->order->status != Order::INITCART) {
+            $this->redirectRoute('front.basket');
+        }
+    }
+
+    #[Computed]
+    public function weeks(): array
+    {
+        $start = Carbon::now()->startOfWeek(Carbon::TUESDAY);
+        $end = Carbon::now()->endOfWeek(Carbon::SATURDAY);
+        $nextStart = Carbon::now()->addWeek()->startOfWeek(Carbon::TUESDAY);
+        $nextEnd = Carbon::now()->addWeek()->endOfWeek(Carbon::SATURDAY);
+
+        return [
+            'current' => [
+                'label' => __('front.slot.little_word.from') . ' ' . $start->format('d/m') . ' ' . __('front.slot.little_word.to') . ' ' . $end->format('d/m'),
+                'days' => CarbonPeriod::create($start, $end),
+            ],
+            'next' => [
+                'label' => __('front.slot.little_word.from') . ' ' . $nextStart->format('d/m') . ' ' . __('front.slot.little_word.to') . ' ' . $nextEnd->format('d/m'),
+                'days' => CarbonPeriod::create($nextStart, $nextEnd),
+            ],
+        ];
+    }
+
+    #[Computed]
+    public function slots()
+    {
+        if (!$this->form->pickup_date) {
+            return [];
+        }
+
+        $dayOfWeek = Carbon::parse($this->form->pickup_date)->dayOfWeekIso;
+
+        $slots = PickupSlot::where('day_iso', $dayOfWeek)
+            ->withCount(['orders' => function ($query) {
+                $query->where('pickup_date', $this->form->pickup_date);
+            }])
+            ->get();
+
+        return $slots;
+    }
+
+    public function createOrder()
+    {
+        if ($this->form->createOrder()) {
+            $this->redirectRoute('front.order-confirmation');
+        }
+    }
+
+    public function isDayPast($day)
+    {
+        return $day->startOfDay() < $this->today->startOfDay();
+    }
+
+    public function isSlotPast($slot)
+    {
+        return !empty($this->form->pickup_date)
+            && $this->form->pickup_date === now()->format('Y-m-d')
+            && $slot->time->format('H:i') <= now()->format('H:i');
+    }
+
+    public function isSlotFull($slot): bool
+    {
+        return $slot->orders_count >= $slot->max_orders;
+    }
+
+    public function switchWeek()
+    {
+        if ($this->selectedWeek == 'current') {
+            $this->selectedWeek = 'next';
+        } else {
+            $this->selectedWeek = 'current';
+        }
+
+    }
+
     public function render()
     {
         return view('livewire.front.slot')
             ->layout('layouts.front')
-            ->title(__('commons.pageName.front.slot').' | Picklio');
+            ->title(__('commons.pageName.front.slot') . ' | Picklio');
     }
 }
--- picklio/resources/views/livewire/front/slot.blade.php
+++ picklio/resources/views/livewire/front/slot.blade.php
@@ -1,3 +1,69 @@
 <div class="slot">
-    <h1>{{__('commons.pageName.front.slot')}}</h1>
+    <section class="slot__container paddingMedia">
+        <h2 class="slot__container__title">{{__('commons.pageName.front.slot')}}</h2>
+        <div class="slot__container__pickupContainer">
+
+            <div class="slot__container__pickupContainer__weekContainer">
+
+                <x-form.select
+                    div-class="slot__container__pickupContainer__weekContainer__inputContainer"
+                    name="selectedWeek"
+                    model-live="selectedWeek"
+                    label="{{__('front.slot.weekContainer.title')}}"
+                    input-class="slot__container__pickupContainer__weekContainer__inputContainer__input"
+                    input-error-class="slot__container__pickupContainer__weekContainer__inputContainer__input__error">
+                    @foreach($this->weeks as $key => $week)
+                        <option value="{{ $key }}">{{ $week['label'] }}</option>
+                    @endforeach
+                </x-form.select>
+                <button wire:click="switchWeek"
+                        class="button button--left slot__container__pickupContainer__weekContainer__button slot__container__pickupContainer__weekContainer__button--left">
+                    <x-svg.svg
+                        class="slot__container__pickupContainer__weekContainer__button__svg slot__container__pickupContainer__weekContainer__button__svg--left"
+                        name="arrow"/>
+                </button>
+                <button wire:click="switchWeek" class="button slot__container__pickupContainer__weekContainer__button">
+                    <x-svg.svg class="slot__container__pickupContainer__weekContainer__button__svg" name="arrow"/>
+                </button>
+            </div>
+            <div class="slot__container__pickupContainer__dayContainer">
+                <p class="slot__container__pickupContainer__dayContainer__title">{{__('front.slot.dayContainer.title')}}</p>
+                <div class="slot__container__pickupContainer__dayContainer__dayCardContainer">
+                    @foreach($this->weeks[$selectedWeek]['days'] as $day)
+                        <button
+                            @disabled($this->isDayPast($day)) wire:click="$set('form.pickup_date', '{{ $day->toDateString() }}')"
+                            class="slot__container__pickupContainer__dayContainer__dayCardContainer__card {{$this->isDayPast($day) ? 'disabled' : ''}} {{ $this->form->pickup_date === $day->toDateString() ? 'active' : '' }}">
+                            <p class="slot__container__pickupContainer__dayContainer__dayCardContainer__card__day">{{ \Carbon\Carbon::parse($day)->translatedFormat('l') }}</p>
+                            <p class="slot__container__pickupContainer__dayContainer__dayCardContainer__card__date">{{ \Carbon\Carbon::parse($day)->format('d') }}</p>
+                            <p class="slot__container__pickupContainer__dayContainer__dayCardContainer__card__month">{{ \Carbon\Carbon::parse($day)->translatedFormat('F') }}</p>
+                        </button>
+                    @endforeach
+                </div>
+                @error('form.pickup_date')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="slot__container__pickupContainer__slotContainer">
+                <p class="slot__container__pickupContainer__slotContainer__title">{{__('front.slot.slotContainer.title')}}</p>
+                <div class="slot__container__pickupContainer__slotContainer__slotCardContainer">
+                    @forelse($this->slots() as $slot)
+                        <button
+                            @disabled($this->isSlotPast($slot) || $this->isSlotFull($slot)) wire:click="$set('form.pickup_slot_id', {{ $slot->id }})"
+                            class="slot__container__pickupContainer__slotContainer__slotCardContainer__card {{ $this->isSlotPast($slot) || $this->isSlotFull($slot) ? 'disabled' : '' }} {{ $this->form->pickup_slot_id === $slot->id ? 'active' : '' }}">
+                            <p class="slot__container__pickupContainer__slotContainer__slotCardContainer__card__time">{{  $slot->time->format('H:i')  }}</p>
+                        </button>
+                    @empty
+                        <p class="slot__container__pickupContainer__slotContainer__slotCardContainer__empty"> {{__('front.slot.empty-slot')}}</p>
+                    @endforelse
+                </div>
+                @error('form.pickup_slot_id')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
+        </div>
+        <button wire:click="createOrder" class="button button--icon slot__container__button">
+            {{__('front.slot.button')}}
+            <x-svg.svg class="slot__container__button__svg" name="arrow"/>
+        </button>
+    </section>
 </div>
