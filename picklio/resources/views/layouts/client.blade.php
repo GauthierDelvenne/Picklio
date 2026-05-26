7a6bf717588800fb35279b2236ecf8861a676085
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2026-05-26 13:47:18 +0200

    fix font-family

--- picklio/resources/views/layouts/client.blade.php
+++ picklio/resources/views/layouts/client.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet">
     <title>{{ $title ?? config('app.name') }}</title>
 
     @vite(['resources/css/admin.css', 'resources/js/app.js'])
